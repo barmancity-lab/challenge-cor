a765e13daf00f7db614352d4c8969d523d3b74e1
Federico Puiggros <fpuiggros@technisys.com>
2021-03-17 10:38:08 -0300

    fix eslint

--- app/schemas/soup-schema.js
+++ app/schemas/soup-schema.js
@@ -4,7 +4,7 @@ const soup = {
   columns: Joi.number().integer().required(),
   rows: Joi.number().integer().required(),
   search: Joi.string().required(),
-  soup: Joi.string().required() 
+  soup: Joi.string().required()
 };
 
 module.exports = soup;
